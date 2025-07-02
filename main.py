bb2728d28a80f0a8c3d1b87349f979317cb84871
hdelcarpiom <140546274+hdelcarpiom@users.noreply.github.com>
2025-07-01 22:54:47 -0500

    Menu selectbox

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import streamlit as st 
+from tables import tables as t
+from charts import charts
+import dataframes
+
+if "menu" not in st.session_state:
+    st.session_state.menu = "Tables"
+
+with st.sidebar:
+    st.session_state.menu= st.sidebar.selectbox("Menu", ["Inicio", "DataFrames", "Charts", "Tables"])
+
+if st.session_state.menu == "Inicio":
+    st.title("Inicio")
+    st.write("Bienvenido a la aplicación de Streamlit con menú.")
+    st.write("Selecciona una opción del menú en la barra lateral para comenzar.")   
+elif st.session_state.menu == "DataFrames":                                    
+    st.title("DataFrames")
+    st.write("Esta es la página de DataFrames.")
+    dataframes.dataframes()
+elif st.session_state.menu == "Tables":
+    st.title("Tables")
+    st.write("Esta es la página de Tables.")
+    t() 
+elif st.session_state.menu == "Charts":
+    st.title("Charts")
+    st.write("Esta es la página de Charts.")
+
+
